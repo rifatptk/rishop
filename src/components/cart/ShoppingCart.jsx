a6aa11163a674deedd6ca1f6e532c7e24ed481a9
rifatptk <rifatptk@gmail.com>
2023-02-17 11:22:30 +0600

    fixed cart issues

--- src/components/cart/ShoppingCart.jsx
+++ src/components/cart/ShoppingCart.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { useDispatch, useSelector } from "react-redux";
@@ -83,123 +83,133 @@ export default function ShoppingCart() {
 
                       <div className="mt-8">
                         <div className="flow-root">
-                          <ul className="-my-6 divide-y divide-gray-200">
-                            {cartItems.map((product) => (
-                              <li key={product.id} className="flex py-6">
-                                <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                                  <img
-                                    src={product.thumbnail}
-                                    alt={product.title}
-                                    className="h-full w-full object-cover object-center"
-                                  />
-                                </div>
-
-                                <div className="ml-4 flex flex-1 flex-col">
-                                  <div>
-                                    <div className="flex justify-between text-base font-medium text-gray-900">
-                                      <h3>
-                                        <Link to={`/products/${product.id}`}>
-                                          {product.title}
-                                        </Link>
-                                      </h3>
-                                      <p className="ml-4">${product.price}</p>
-                                    </div>
-                                    <p className="mt-1 text-sm text-gray-500">
-                                      {product.category}
-                                    </p>
+                          {!!cartItems.length ? (
+                            <ul className="-my-6 divide-y divide-gray-200">
+                              {cartItems.map((product) => (
+                                <li key={product.id} className="flex py-6">
+                                  <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                                    <img
+                                      src={product.thumbnail}
+                                      alt={product.title}
+                                      className="h-full w-full object-cover object-center"
+                                    />
                                   </div>
-                                  <div className="flex flex-1 items-end justify-between text-sm">
-                                    <p className="text-gray-500">
-                                      Qty{" "}
-                                      <button
-                                        disabled={product.quantity < 2}
-                                        onClick={() =>
-                                          dispatch(
-                                            decrementQuantity({
-                                              productId: product.id,
-                                            })
-                                          )
-                                        }
-                                        className={`px-3 rounded mx-3 ${
-                                          product.quantity < 2
-                                            ? "bg-gray-300"
-                                            : "bg-blue-500"
-                                        } text-white font-bold`}
-                                      >
-                                        -
-                                      </button>
-                                      {product.quantity}{" "}
-                                      <button
-                                        disabled={
-                                          product.quantity >= product.stock
-                                        }
-                                        onClick={() =>
-                                          dispatch(
-                                            incrementQuantity({
-                                              productId: product.id,
-                                            })
-                                          )
-                                        }
-                                        className={`px-3 rounded ml-3 ${
-                                          product.quantity >= product.stock
-                                            ? "bg-gray-300"
-                                            : "bg-blue-500"
-                                        } text-white font-bold`}
-                                      >
-                                        +
-                                      </button>
-                                    </p>
 
-                                    <div className="flex">
-                                      <button
-                                        onClick={() =>
-                                          dispatch(removeFromCart(product.id))
-                                        }
-                                        type="button"
-                                        className="font-medium text-indigo-600 hover:text-indigo-500"
-                                      >
-                                        Remove
-                                      </button>
+                                  <div className="ml-4 flex flex-1 flex-col">
+                                    <div>
+                                      <div className="flex justify-between text-base font-medium text-gray-900">
+                                        <h3>
+                                          <Link to={`/products/${product.id}`}>
+                                            {product.title}
+                                          </Link>
+                                        </h3>
+                                        <p className="ml-4">${product.price}</p>
+                                      </div>
+                                      <p className="mt-1 text-sm text-gray-500">
+                                        {product.brand} - {product.category}
+                                      </p>
+                                    </div>
+                                    <div className="flex flex-1 items-end justify-between text-sm">
+                                      <p className="text-gray-500">
+                                        Qty{" "}
+                                        <button
+                                          disabled={product.quantity < 2}
+                                          onClick={() =>
+                                            dispatch(
+                                              decrementQuantity({
+                                                productId: product.id,
+                                              })
+                                            )
+                                          }
+                                          className={`px-3 rounded mx-3 ${
+                                            product.quantity < 2
+                                              ? "bg-gray-300"
+                                              : "bg-blue-500"
+                                          } text-white font-bold`}
+                                        >
+                                          -
+                                        </button>
+                                        {product.quantity}{" "}
+                                        <button
+                                          disabled={
+                                            product.quantity >= product.stock
+                                          }
+                                          onClick={() =>
+                                            dispatch(
+                                              incrementQuantity({
+                                                productId: product.id,
+                                              })
+                                            )
+                                          }
+                                          className={`px-3 rounded ml-3 ${
+                                            product.quantity >= product.stock
+                                              ? "bg-gray-300"
+                                              : "bg-blue-500"
+                                          } text-white font-bold`}
+                                        >
+                                          +
+                                        </button>
+                                      </p>
+
+                                      <div className="flex">
+                                        <button
+                                          onClick={() =>
+                                            dispatch(removeFromCart(product.id))
+                                          }
+                                          type="button"
+                                          className="font-medium text-indigo-600 hover:text-indigo-500"
+                                        >
+                                          Remove
+                                        </button>
+                                      </div>
                                     </div>
                                   </div>
-                                </div>
-                              </li>
-                            ))}
-                          </ul>
+                                </li>
+                              ))}
+                            </ul>
+                          ) : (
+                            <div>
+                              <h3 className="text-red-500">
+                                Your cart is empty!
+                              </h3>
+                            </div>
+                          )}
                         </div>
                       </div>
                     </div>
-
-                    <div className="border-t border-gray-200 py-6 px-4 sm:px-6">
-                      <div className="flex justify-between text-base font-medium text-gray-900">
-                        <p>Subtotal</p>
-                        <p>${totalPrice}</p>
-                      </div>
-                      <p className="mt-0.5 text-sm text-gray-500">
-                        Shipping and taxes calculated at checkout.
-                      </p>
-                      <div className="mt-6">
-                        <Link
-                          to="/checkout"
-                          className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
-                        >
-                          Checkout
-                        </Link>
-                      </div>
-                      <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
-                        <p>
-                          or
-                          <button
-                            type="button"
-                            className="font-medium text-indigo-600 hover:text-indigo-500"
-                            onClick={() => dispatch(toggleCart())}
-                          >
-                            Continue Shopping
-                            <span aria-hidden="true"> &rarr;</span>
-                          </button>
+                    {!!cartItems.length && (
+                      <div className="border-t border-gray-200 py-6 px-4 sm:px-6">
+                        <div className="flex justify-between text-base font-medium text-gray-900">
+                          <p>Subtotal</p>
+                          <p>${totalPrice}</p>
+                        </div>
+                        <p className="mt-0.5 text-sm text-gray-500">
+                          Shipping and taxes calculated at checkout.
                         </p>
+                        <div className="mt-6">
+                          <Link
+                            onClick={() => dispatch(toggleCart())}
+                            to="/checkout"
+                            className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                          >
+                            Checkout
+                          </Link>
+                        </div>
+                        <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+                          <p>
+                            or
+                            <button
+                              type="button"
+                              className="font-medium text-indigo-600 hover:text-indigo-500"
+                              onClick={() => dispatch(toggleCart())}
+                            >
+                              Continue Shopping
+                              <span aria-hidden="true"> &rarr;</span>
+                            </button>
+                          </p>
+                        </div>
                       </div>
-                    </div>
+                    )}
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
