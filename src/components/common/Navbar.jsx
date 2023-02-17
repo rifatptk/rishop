2ab33daa2bd8b9f2b45e15d91776d5b46154ae88
rifatptk <rifatptk@gmail.com>
2023-02-17 08:14:04 +0600

    product overview page done

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,6 +1,11 @@
 import { Fragment, useState } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import {
+  Bars3Icon,
+  BellIcon,
+  ShoppingBagIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
 
 const navigation = [
@@ -66,8 +71,8 @@ export default function Navbar() {
                   type="button"
                   className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                 >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                  <span className="sr-only">View cart</span>
+                  <ShoppingBagIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
 
                 {/* Profile dropdown */}
--- src/components/productDetail/ProductOverview.jsx
+++ src/components/productDetail/ProductOverview.jsx
@@ -1,63 +1,10 @@
-import { useState } from "react";
-import { StarIcon } from "@heroicons/react/20/solid";
-import { RadioGroup } from "@headlessui/react";
 import useGetData from "../../hooks/useGetData";
 import { useParams } from "react-router-dom";
 import { HashLoader } from "react-spinners";
 import RiSlider from "./RiSlider";
+import { StarIcon } from "@heroicons/react/24/solid";
+import { StarIcon as StarOutlinedIcon } from "@heroicons/react/24/outline";
 
-const product = {
-  name: "Basic Tee 6-Pack",
-  price: "$192",
-  href: "#",
-  breadcrumbs: [
-    { id: 1, name: "Men", href: "#" },
-    { id: 2, name: "Clothing", href: "#" },
-  ],
-  images: [
-    {
-      src: "https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg",
-      alt: "Two each of gray, white, and black shirts laying flat.",
-    },
-    {
-      src: "https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg",
-      alt: "Model wearing plain black basic tee.",
-    },
-    {
-      src: "https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg",
-      alt: "Model wearing plain gray basic tee.",
-    },
-    {
-      src: "https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg",
-      alt: "Model wearing plain white basic tee.",
-    },
-  ],
-  colors: [
-    { name: "White", class: "bg-white", selectedClass: "ring-gray-400" },
-    { name: "Gray", class: "bg-gray-200", selectedClass: "ring-gray-400" },
-    { name: "Black", class: "bg-gray-900", selectedClass: "ring-gray-900" },
-  ],
-  sizes: [
-    { name: "XXS", inStock: false },
-    { name: "XS", inStock: true },
-    { name: "S", inStock: true },
-    { name: "M", inStock: true },
-    { name: "L", inStock: true },
-    { name: "XL", inStock: true },
-    { name: "2XL", inStock: true },
-    { name: "3XL", inStock: true },
-  ],
-  description:
-    'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
-  highlights: [
-    "Hand cut and sewn locally",
-    "Dyed with our proprietary colors",
-    "Pre-washed & pre-shrunk",
-    "Ultra-soft 100% cotton",
-  ],
-  details:
-    'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
-};
 const reviews = { href: "#", average: 4, totalCount: 117 };
 
 function classNames(...classes) {
@@ -66,10 +13,30 @@ function classNames(...classes) {
 
 export default function ProductOverview() {
   const { productId } = useParams();
-  const { data, isLoading, error } = useGetData({
+
+  const {
+    data: product,
+    isLoading,
+    error,
+  } = useGetData({
     endpoint: `/products/${productId}`,
   });
-  console.log(data);
+
+  function renderRating() {
+    const ratings = new Array(Math.floor(product.rating)).fill(true);
+    while (ratings.length < 5) {
+      ratings.push(false);
+    }
+    return (
+      <div className="flex">
+        {ratings.map((el, i) => (
+          <span key={i} className="text-yellow-500 [&>svg]:w-5">
+            {el ? <StarIcon /> : <StarOutlinedIcon />}
+          </span>
+        ))}
+      </div>
+    );
+  }
   if (isLoading) {
     return (
       <div className="h-screen grid place-items-center">
@@ -92,15 +59,15 @@ export default function ProductOverview() {
     <div className="pt-6 container mx-auto">
       {/* Image gallery */}
       <div className="lg:flex [&>div]:flex-1">
-        <div className="">
-          <RiSlider items={data.images} />
+        <div>
+          <RiSlider items={product.images} />
         </div>
 
         {/* Product info */}
         <div className="mx-auto max-w-2xl px-4 pt-10 pb-16 sm:px-6 lg:px-8 lg:pt-16 lg:pb-24">
           <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
             <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
-              {product.name}
+              {product.title}
             </h1>
           </div>
 
@@ -108,33 +75,20 @@ export default function ProductOverview() {
           <div className="mt-4 lg:row-span-3 lg:mt-0">
             <h2 className="sr-only">Product information</h2>
             <p className="text-3xl tracking-tight text-gray-900">
-              {product.price}
+              ${product.price}
             </p>
 
             {/* Reviews */}
             <div className="mt-6">
               <h3 className="sr-only">Reviews</h3>
               <div className="flex items-center">
-                <div className="flex items-center">
-                  {[0, 1, 2, 3, 4].map((rating) => (
-                    <StarIcon
-                      key={rating}
-                      className={classNames(
-                        reviews.average > rating
-                          ? "text-gray-900"
-                          : "text-gray-200",
-                        "h-5 w-5 flex-shrink-0"
-                      )}
-                      aria-hidden="true"
-                    />
-                  ))}
-                </div>
-                <p className="sr-only">{reviews.average} out of 5 stars</p>
+                {renderRating()}
+                <p className="sr-only">{product.rating} out of 5 stars</p>
                 <a
                   href={reviews.href}
                   className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500"
                 >
-                  {reviews.totalCount} reviews
+                  237
                 </a>
               </div>
             </div>
@@ -154,21 +108,71 @@ export default function ProductOverview() {
               <h3 className="text-sm font-medium text-gray-900">Highlights</h3>
 
               <div className="mt-4">
-                <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
-                  {product.highlights.map((highlight) => (
-                    <li key={highlight} className="text-gray-400">
-                      <span className="text-gray-600">{highlight}</span>
-                    </li>
-                  ))}
+                <ul className="list-disc space-y-2 pl-4 text-sm">
+                  <li className="text-gray-400">
+                    <span className="text-gray-600">
+                      100% Authentic product
+                    </span>
+                  </li>
+                  <li className="text-gray-400">
+                    <span className="text-gray-600">
+                      7 Days Money return guarantee
+                    </span>
+                  </li>
+                  <li className="text-gray-400">
+                    <span className="text-gray-600">24/7 Customer support</span>
+                  </li>
                 </ul>
               </div>
+            </div>
+            <div className="flex gap-5 mt-5">
+              <button className="bg-green-600 text-white font-bold px-5 py-2 rounded">
+                Buy now
+              </button>
+              <button className="bg-blue-500 text-white font-bold px-5 py-2 rounded">
+                Add to cart
+              </button>
             </div>
 
             <div className="mt-10">
               <h2 className="text-sm font-medium text-gray-900">Details</h2>
 
-              <div className="mt-4 space-y-6">
-                <p className="text-sm text-gray-600">{product.details}</p>
+              <div className="mt-4 space-y-6 text-sm text-gray-600">
+                <p>{product.description}</p>
+                <p>
+                  Et reiciendis cupiditate nemo ullam laborum? Recusandae,
+                  distinctio voluptatibus. Quisquam dignissimos, accusamus rem
+                  repellendus deleniti minus maxime mollitia architecto magnam
+                  minima alias ad autem amet, omnis adipisci impedit, fuga sint
+                  asperiores reprehenderit? Autem, sunt vero? Quidem consectetur
+                  autem temporibus voluptas nam minus harum, illo facilis
+                  accusamus, praesentium illum repellendus. Officia beatae
+                  perspiciatis inventore id modi molestiae consequatur deserunt
+                  in odit veniam, minima optio repudiandae quasi, iste aperiam!
+                  Asperiores quisquam nam dolore sapiente beatae quis, cum nobis
+                  rerum, quod quas molestias mollitia aliquam minus perferendis
+                  maxime magni eaque, ipsam accusantium. Sit non amet expedita
+                  distinctio odio nisi enim officia ducimus ullam suscipit qui,
+                  quod cumque sint at inventore sapiente itaque ipsa? Quibusdam
+                  autem iure placeat, odit quia cum neque voluptatum totam
+                  excepturi dignissimos deserunt molestiae esse voluptatibus
+                  ullam?
+                </p>
+                <p>
+                  Eius repellendus eligendi quasi recusandae iure provident
+                  totam quod quae a excepturi molestiae enim magnam quidem
+                  labore expedita, officia, repudiandae nobis placeat impedit
+                  corporis quo? Facere laudantium quibusdam a vero laboriosam
+                  obcaecati quam perferendis, quidem, eaque nostrum molestiae
+                  est voluptas quis quasi odio porro fuga! Quam assumenda vitae
+                  animi alias doloribus. Vitae placeat repellendus odio minus
+                  qui. Pariatur autem velit voluptas? Numquam eaque suscipit,
+                  animi necessitatibus iste unde qui excepturi quaerat hic
+                  placeat corporis maiores nemo adipisci at eveniet pariatur
+                  vitae, explicabo impedit harum porro quod deleniti quam
+                  debitis? Sed obcaecati fugit eligendi dolor voluptates ratione
+                  maxime sapiente.
+                </p>
               </div>
             </div>
           </div>
