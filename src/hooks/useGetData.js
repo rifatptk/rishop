1f6ebe2a7d17d2f176390f69ccab5e7278477173
rifatptk <rifatptk@gmail.com>
2023-02-17 21:46:39 +0600

    Added jsdoc to useGetData custom hook

--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -1,6 +1,16 @@
 import { useEffect, useState } from "react";
 import { axiosInstance } from "../apiConfigs/axiosInstances";
 
+/**
+
+Retrieves data from an API endpoint using Axios.
+@param {Object} options - The options object.
+@param {string} options.endpoint - The URL of the API endpoint to retrieve data from.
+@param {string|null} options.title - The title of the data.
+@returns {Object} An object containing the retrieved data, loading state, and any errors that occurred.
+
+*/
+
 export default function useGetData(options) {
   const title = options.title || null;
   const [data, setData] = useState(null);
